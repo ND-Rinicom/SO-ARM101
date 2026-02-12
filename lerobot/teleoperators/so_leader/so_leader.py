f2950ae9ede06f9cb9f02e440a534e34ab7fd4fd
ND-Rinicom <ndorring@gmail.com>
2026-02-12 14:00:39 +0000

    Removed wrist callabration

--- lerobot/teleoperators/so_leader/so_leader.py
+++ lerobot/teleoperators/so_leader/so_leader.py
@@ -101,12 +101,15 @@ class SOLeader(Teleoperator):
         input(f"Move {self} to the middle of its range of motion and press ENTER....")
         homing_offsets = self.bus.set_half_turn_homings()
 
-        # Record ranges of motion for all motors
+        full_turn_motor = "wrist_roll"
+        unknown_range_motors = [motor for motor in self.bus.motors if motor != full_turn_motor]
         print(
-            "Move all joints sequentially through their entire ranges of motion.\n"
-            "Recording positions. Press ENTER to stop..."
+            f"Move all joints except '{full_turn_motor}' sequentially through their "
+            "entire ranges of motion.\nRecording positions. Press ENTER to stop..."
         )
-        range_mins, range_maxes = self.bus.record_ranges_of_motion(list(self.bus.motors))
+        range_mins, range_maxes = self.bus.record_ranges_of_motion(unknown_range_motors)
+        range_mins[full_turn_motor] = 0
+        range_maxes[full_turn_motor] = 4095
 
         self.calibration = {}
         for motor, m in self.bus.motors.items():
